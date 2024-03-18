8817f4eb930528b07bc15e5b4a1062e405c0b2fa
Duong Nguyen <duong.nguyen@bliksund.com>
2024-03-18 16:31:11 +0700

    some more kata

---
+++ PHP/8kyu/correctTheMistakesOfTheCharacterRecognitionSoftware.php
@@ -0,0 +1,7 @@
+<?php
+
+//https://www.codewars.com/kata/577bd026df78c19bca0002c0
+
+function correct($string) {
+    return str_replace(['5', '0', '1'], ['S', 'O', 'I'], $string);
+}
---
+++ PHP/8kyu/welcome.php
@@ -0,0 +1,27 @@
+<?php
+
+//https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+function greet(string $language): string {
+    $text = [
+        'english' => 'Welcome',
+        'czech' => 'Vitejte',
+        'danish' => 'Velkomst',
+        'dutch' => 'Welkom',
+        'estonian' => 'Tere tulemast',
+        'finnish' => 'Tervetuloa',
+        'flemish' => 'Welgekomen',
+        'french' => 'Bienvenue',
+        'german' => 'Willkommen',
+        'irish' => 'Failte',
+        'italian' => 'Benvenuto',
+        'latvian' => 'Gaidits',
+        'lithuanian' => 'Laukiamas',
+        'polish' => 'Witamy',
+        'spanish' => 'Bienvenido',
+        'swedish' => 'Valkommen',
+        'welsh' => 'Croeso',
+    ];
+
+    return $text[$language] ?? 'Welcome';
+}
